dbc7c26e8859318cefef4b4567ef89671608d1a2
Saumya Pandey <sp160899@gmail.com>
2019-12-20 23:38:37 +0530

    Add DOM manipulation
    It includes playing the game and has first to five wins functionality

--- js/script.js
+++ js/script.js
@@ -1,53 +1,70 @@
-          function playRound(playerSelection,computerSelection){
-                playerSelection = playerSelection.toLowerCase();
-                if(playerSelection==="rock" && computerSelection==="paper"){
-                    return 0;
-                }
-                else if(playerSelection==="rock" && computerSelection==="scissor"){
-                    return 1;
-                }
-                else if(playerSelection==="paper" && computerSelection==="rock"){
-                    return 1;
-                }
-                else if(playerSelection==="paper" && computerSelection==="scissor"){
-                    return 0;
-                }
-                else if(playerSelection==="scissor" && computerSelection==="paper"){
-                    return 1;
-                }
-                else if(playerSelection==="scissor" && computerSelection==="rock"){
-                    return 0;
-                }
-            }
-            
-            function game(){
-                
-                let user = 0 ;
-                let computer = 0;
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        return 0;
+    } else if (playerSelection === "rock" && computerSelection === "scissor") {
+        return 1;
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return 1;
+    } else if (playerSelection === "paper" && computerSelection === "scissor") {
+        return 0;
+    } else if (playerSelection === "scissor" && computerSelection === "paper") {
+        return 1;
+    } else if (playerSelection === "scissor" && computerSelection === "rock") {
+        return 0;
+    }
 
-                for(let i=0;i<5;i++){
-                    const playerSelection = prompt("Enter your choice","");
-                    const computerSelection = computerPlay()
-                    let result = playRound(playerSelection, computerSelection)
-                    if(result==1)
-                        user++;
-                    else if(result==0)
-                        computer++;
-                    
-                    console.log(`User ${user} |||| Computer ${computer}.`);
-                }
+    return;
+}
 
-                if(user>computer){
-                    console.log("You Win!");
-                }
-                else if(user<computer){
-                    console.log("You Lose!");
-                }
-                else
-                    console.log("It is a Draw!");
-            }
-            function computerPlay(){
-                let randomnumber = Math.floor(Math.random()*3);
-                var choices = ['rock','paper','scissor'];
-                return choices[randomnumber];
-            }
+let user = 0;
+let computer = 0;
+const btns = document.querySelectorAll(".youbtn");
+btns.forEach(youbtn => youbtn.addEventListener("click", checkAfterClick));
+
+function checkAfterClick(e) {
+
+    const status = document.getElementById("status");
+    const playerSelection = this.getAttribute("id");
+    const computerSelection = computerPlay();
+    let result = playRound(playerSelection, computerSelection)
+    if (result == 1) {
+        user++;
+        status.textContent = "You Win!";
+    } else if (result == 0) {
+        computer++;
+        status.textContent = "You Lose!";
+    } else {
+        status.textContent = "It is a Draw";
+    }
+
+    const youcounter = document.getElementById("youcounter");
+    youcounter.textContent = `${user}`;
+    const machinecounter = document.getElementById("machinecounter");
+    machinecounter.textContent = `${computer}`;
+
+    if (user === 5 || computer === 5) {
+        if (user === 5) {
+            status.textContent = "You won the Championship <3";
+        } else {
+            status.textContent = "Better luck next time!";
+        }
+    }
+}
+
+function reset() {
+    user = 0;
+    computer = 0;
+    const youcounter = document.getElementById("youcounter");
+    youcounter.textContent = "0";
+    const machinecounter = document.getElementById("machinecounter");
+    machinecounter.textContent = "0";
+    const status = document.getElementById("status");
+    status.textContent = "";
+}
+
+function computerPlay() {
+    let randomnumber = Math.floor(Math.random() * 3);
+    var choices = ['rock', 'paper', 'scissor'];
+    return choices[randomnumber];
+}
